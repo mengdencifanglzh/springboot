f6c4693e53fded0008d95a154acc5667ff0ddad1
java17lzh <574003068@qq.com>
2018-10-17 10:40:52 +0800

    第一次eclipse提交

---
+++ src/main/java/com/accp/Springbootgit111Application.java
@@ -0,0 +1,12 @@
+package com.accp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootgit111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootgit111Application.class, args);
+	}
+}
